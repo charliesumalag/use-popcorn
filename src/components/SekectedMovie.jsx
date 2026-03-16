dd2c9ef18a082e43b26607089220119d974bc398
Charlie Sumalag <charliesumalag444@yahoo.com.ph>
2026-03-16 22:49:23 +0800

    addig a new effect: changing page title

--- src/components/SekectedMovie.jsx
+++ src/components/SekectedMovie.jsx
@@ -56,6 +56,12 @@ const SekectedMovie = ({selectedId, onCloseMovie, onAddWatched, watched}) => {
     }, [selectedId])
 
 
+    useEffect(function () {
+        if(!title) return;
+        document.title = `Movie: ${title}`;
+    },[title])
+
+
   return (
     <div className='details'>
         {isLoading ? <Loader /> :
